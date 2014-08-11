bd2679b5bf8e0e1caa4a62c95490294eee5c2fae
Xinyue Zhao <czhao@cellular.de>
2014-08-11 09:55:27 +0200

    Added normal event-handler for NoStickyFragment.

--- EventBusSample/src/main/java/com/kevintan/eventbussample/fragments/NoStickyFragment.java
+++ EventBusSample/src/main/java/com/kevintan/eventbussample/fragments/NoStickyFragment.java
@@ -1,6 +1,7 @@
 package com.kevintan.eventbussample.fragments;
 
 import com.kevintan.eventbussample.R;
+import com.kevintan.eventbussample.bus.NormalEvent;
 import com.kevintan.eventbussample.bus.StickyEvent;
 import com.kevintan.eventbussample.bus.UpdateActionBarTitleEvent;
 
@@ -12,8 +13,9 @@ import android.widget.TextView;
 import de.greenrobot.event.EventBus;
 
 /**
- * {@link NoStickyFragment} can not accept a sticky-event, UI can't refresh although it implements handler that handles
- * the {@link com.kevintan.eventbussample.bus.StickyEvent}.
+ * {@link NoStickyFragment} can not accept a sticky-event, but normal event is no problem.
+ * <p/>
+ * UI can't refresh although it implements handler that handles the {@link com.kevintan.eventbussample.bus.StickyEvent}.
  */
 public final class NoStickyFragment extends BaseFragment {
 	private static final int LAYOUT = R.layout.fragment_no_sticky;
@@ -25,10 +27,20 @@ public final class NoStickyFragment extends BaseFragment {
 	 * 		Event {@link  com.kevintan.eventbussample.bus.StickyEvent}.
 	 */
 	public void onEvent(StickyEvent e) {
-		if (getView() instanceof TextView) {
-			TextView textView = (TextView) getView();
-			textView.setText(e.getDataObject().toString());
-		}
+		TextView textView = (TextView) getView().findViewById(R.id.text_tv);
+		textView.setText(e.getDataObject().toString());
+	}
+
+	/**
+	 * Handler for {@link com.kevintan.eventbussample.bus.NormalEvent}
+	 *
+	 * @param e
+	 * 		Event {@link com.kevintan.eventbussample.bus.NormalEvent}.
+	 */
+	public void onEvent(NormalEvent e) {
+		TextView textView = (TextView) getView().findViewById(R.id.text_tv);
+		textView.setText("Got a normal event.");
+
 	}
 
 
@@ -38,5 +50,17 @@ public final class NoStickyFragment extends BaseFragment {
 		/*Update ActionBar's title.*/
 		EventBus.getDefault().post(new UpdateActionBarTitleEvent(getString(R.string.screen_4)));
 		return inflater.inflate(LAYOUT, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		view.findViewById(R.id.normal_event_btn).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*Can send a normal event and handle it.*/
+				EventBus.getDefault().post(new NormalEvent());
+			}
+		});
 	}
 }
